d849afd0d808c6712adcb67266cf997fb6b4fec0
zhoujun <dev.zhoujun@gmail.com>
2016-07-31 20:00:57 +0800

    Example

--- example/main.go
+++ example/main.go
@@ -7,16 +7,28 @@ import (
 	"github.com/4396/utmd"
 )
 
-func main() {
-	tor, _ := dht.ResolveID("497673c9d0ca952492dc3a092115ac587e3a01d9")
-	p, err := utmd.Handshake("111.182.197.21:11101", tor.Bytes())
+func download(s string) (b []byte, err error) {
+	tor, err := dht.ResolveID(s[:40])
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	defer p.Close()
-	fmt.Println("----------")
-	if err = p.Download(); err != nil {
-		fmt.Println(err)
+	b, err = utmd.Download(s[41:], tor.Bytes(), func(down, total int) {
+		fmt.Println(s, down, total)
+	})
+	return
+}
+
+func tryDownload(s string, n int) (data []byte, err error) {
+	for i := 0; i < n; i++ {
+		data, err = download(s)
+		if err == nil {
+			return
+		}
 	}
+	return
+}
+
+func main() {
+	data, err := tryDownload("497673c9d0ca952492dc3a092115ac587e3a01d9 111.182.197.21:11101", 3)
+	fmt.Println(err, string(data))
 }
